10df8464dd92cf9101afb9d3874cd9197d11d16d
Muhammad Shoaib <94863313+shoaib7619@users.noreply.github.com>
2024-03-19 19:54:31 +0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import Navbar from "./components/Navbar/navbar"
-// import Intro from "./components/Intro/intro";
-// import Skills from "./components/Skills/skills"
-// import Works from "./components/Works/work";
-// import Contact from "./components/Contact/contact";
-// import Footer from "./components/Footer/footer";
+import Intro from "./components/Intro/intro";
+import Skills from "./components/Skills/skills"
+import Works from "./components/Works/work";
+import Contact from "./components/Contact/contact";
+import Footer from "./components/Footer/footer";
 function App() {
 
   return (
     <>
     <Navbar/>
-   {/* <Intro/> */}
-     {/* <Skills/>
+   <Intro/>
+     <Skills/>
     <Works/>
     <Contact/>
-    <Footer/> */}
+    <Footer/>
     </>
     
   )
